13cfe6301a119502e912094f86b1d0c7e01b6ec4
NSnirjash <158472502+NSnirjash@users.noreply.github.com>
2024-10-07 18:32:37 +0600

    add

--- AndroidAppJee59/app/src/main/java/com/nsnirjash/androidappjee59/HomeActivity.java
+++ AndroidAppJee59/app/src/main/java/com/nsnirjash/androidappjee59/HomeActivity.java
@@ -20,5 +20,18 @@ public class HomeActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+//        val imageList = ArrayList<SlideModel>() // Create image list
+
+// imageList.add(SlideModel("String Url" or R.drawable)
+// imageList.add(SlideModel("String Url" or R.drawable, "title") You can add title
+
+//        imageList.add(SlideModel("https://bit.ly/2YoJ77H", "The animal population decreased by 58 percent in 42 years."))
+//        imageList.add(SlideModel("https://bit.ly/2BteuF2", "Elephants and tigers may become extinct."))
+//        imageList.add(SlideModel("https://bit.ly/3fLJf72", "And people do that."))
+//
+//        val imageSlider = findViewById<ImageSlider>(R.id.image_slider)
+//                imageSlider.setImageList(imageList)
     }
 }
