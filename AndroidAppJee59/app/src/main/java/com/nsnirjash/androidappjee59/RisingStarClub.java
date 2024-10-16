4916e936e78de4307e45d07b80d311ee553cec96
NSnirjash <158472502+NSnirjash@users.noreply.github.com>
2024-10-16 19:10:27 +0600

    add

--- AndroidAppJee59/app/src/main/java/com/nsnirjash/androidappjee59/RisingStarClub.java
+++ AndroidAppJee59/app/src/main/java/com/nsnirjash/androidappjee59/RisingStarClub.java
@@ -7,8 +7,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.nsnirjash.androidappjee59.ApiClient.ApiClient;
+import com.nsnirjash.androidappjee59.adapter.PurbachalModelAdapter;
+import com.nsnirjash.androidappjee59.adapter.RisingStarAdapter;
+import com.nsnirjash.androidappjee59.api.PurbachalClubApi;
+import com.nsnirjash.androidappjee59.api.RisingStarApi;
+import com.nsnirjash.androidappjee59.model.PurbachalModel;
+
+import java.util.List;
+
+import retrofit2.Call;
 
 public class RisingStarClub extends AppCompatActivity {
+
+    private RecyclerView risingStarRecyclerView;
+
+    private RisingStarAdapter risingStarAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +37,14 @@ public class RisingStarClub extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        risingStarRecyclerView=findViewById(R.id.risingStarRecyclerView);
+
+        risingStarRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        RisingStarApi clubApi = ApiClient.getRetrofit().create(RisingStarApi.class);
+
+
     }
 }
